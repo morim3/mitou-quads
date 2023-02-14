a3917f0ad6fa39fc46938922d4e7f085ee037315
Kohei Morimoto <acetaminophen151@gmail.com>
2023-02-14 15:26:12 +0900

    add objective functions

--- utils/objective_functions.py
+++ utils/objective_functions.py
@@ -12,17 +12,20 @@ def get_squared(target, square_term=1):
         return  square_term * np.sum((x - target[..., :]) ** 2, axis=-1)
     return squared
 
-def get_styblinski_tang():
-    
-    def base_fun(x, s):
-        return np.sum(x**4 - 16 * x**2 + 5 * x, axis=-1) / 2 / s
+def get_styblinski_tang(dim):
+
+    target = np.array([-2.90353402777118 / 10 + 0.5] * dim)
+    def base_fun(x):
+        return np.sum(x**4 - 16 * x**2 + 5 * x, axis=-1) / 2
     def styblinski_tang(x):
-        optimal_point = -2.90353402777118 * np.ones_like(y)
+        optimal_point = -2.90353402777118 * np.ones_like(x)
+
         # x in [0,1] -> y in [-5, 5]
+        # optimal_point = 0.20964659722
         y = (x - 0.5) * 10
         # offset optimal value
         return base_fun(y) - base_fun(optimal_point)
-    return styblinski_tang
+    return styblinski_tang, target
 
 def rosenbrock(x):
     return np.sum((x[:, 1:] - x[:, :-1] ** 2) ** 2 * 100 + (1 - x[:, :-1])**2)
