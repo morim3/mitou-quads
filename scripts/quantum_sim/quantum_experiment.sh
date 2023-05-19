e7c1f64c15a088c1ac8b426a9bce8f7216db3a8f
Kohei Morimoto <acetaminophen151@gmail.com>
2023-05-19 19:32:16 +0900

    change experiment setting

--- scripts/quantum_sim/quantum_experiment.sh
+++ scripts/quantum_sim/quantum_experiment.sh
@@ -1,10 +1,20 @@
 
 
-for fun in rastrigin schwefel styblinski_tang easom squared ackley griewank
-  do
-    for method in cmaes quads grover
+for dim in 1 2 3
+do
+  for fun in rastrigin schwefel styblinski_tang squared ackley griewank rosenbrock xin_she_yang
     do
-      tsp python models/run_methods.py --name ${method}_${fun}_3 --method $method --func $fun --n_dim 3 --sampler_type quantum --eval_limit_per_update 10000000000 --group $fun --project_name mitou-quads-quantum --entity preview-control
-    done
+      for method in cmaes quads grover adam
+      do
+        tsp python models/run_methods.py --name ${method}_${fun}_${dim} --method $method --func $fun --n_dim $dim --sampler_type quantum --eval_limit_per_update 10000000000 --group $fun --project_name mitou-quads-quantum2 --entity preview-control
+      done
+  done
 done
 
+for fun in rastrigin schwefel styblinski_tang squared ackley griewank rosenbrock xin_she_yang
+do
+  for method in cmaes quads grover adam
+  do
+    tsp python models/run_methods.py --name ${method}_${fun}_4 --method $method --func $fun --n_dim 4 --sampler_type quantum --eval_limit_per_update 10000000000 --group $fun --project_name mitou-quads-quantum2 --entity preview-control --n_digits 7
+  done
+done
