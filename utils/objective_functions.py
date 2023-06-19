bfcaa5336db803be5597ba4d961cb3f829e70c6b
morim3 <acetaminophen151@gmail.com>
2023-06-19 10:07:56 +0900

    add funcs

--- utils/objective_functions.py
+++ utils/objective_functions.py
@@ -95,7 +95,66 @@ def get_xin_she_yang(dim=3, target=None, use_jax=False):
         return np.sum(np.abs(x), axis=-1) * np.exp(-np.sum(x**2, axis=-1))
     return fun, target
 
+def alpine01(dim=3, target=None, use_jax=False):
+    np = importlib.import_module('jax.numpy' if use_jax else 'numpy')
+    if target is None:
+        target = np.ones(dim, dtype=np.float32) * 0.5
 
+    def fun(x):
+        x = (x-target[None]) * 20
+        return np.sum(np.abs(x*np.sin(x)+0.1*x), axis=-1)
+    return fun, target
+    
+def alpine02(dim=3, target=None, use_jax=False):
+    np = importlib.import_module('jax.numpy' if use_jax else 'numpy')
+    target = np.ones(dim, dtype=np.float32) * 0.7917
+
+    def fun(x):
+        x = x * 10
+        return -np.prod(np.sqrt(x)*np.sin(x), axis=-1)
+    return fun, target
+
+
+def levy(dim=3, target=None, use_jax=False):
+    np = importlib.import_module('jax.numpy' if use_jax else 'numpy')
+    target = np.ones(dim, dtype=np.float32) * 0.55
+
+    def fun(x):
+        x = (x-0.5) * 20
+        x = 1 + (x - 1) / 4
+        return np.sin(np.pi*x[:, 0])**2+np.sum((x[:, :-1]-1)**2*(1+10*np.sin(np.pi*x[:, 1:])**2)) + (x[:, -1]-1)**2 * (1+np.sin(2*np.pi*x[:, -1]))
+
+    return fun, target
+
+def mishra(dim=3, target=None, use_jax=False):
+    np = importlib.import_module('jax.numpy' if use_jax else 'numpy')
+    target = np.ones(dim, dtype=np.float32) * 0.5
+
+    def fun(x):
+        x = (x-0.5) * 20
+        return (np.mean(np.abs(x), axis=-1)  - np.power(np.prod(np.abs(x), axis=-1), 1/dim))**2
+
+    return fun, target
+
+def deflectedCorrugatedSpring(dim=3, target=None, use_jax=False):
+    np = importlib.import_module('jax.numpy' if use_jax else 'numpy')
+    target = np.ones(dim, dtype=np.float32) * 0.5
+
+    def fun(x):
+        x = x * 10
+        return 0.1*np.sum((x-5)**2-np.cos(5*np.sqrt(np.sum((x-5)**2, axis=-1, keepdims=True))), axis=-1)
+
+    return fun, target
+
+def wavy(dim=3, target=None, use_jax=False):
+    np = importlib.import_module('jax.numpy' if use_jax else 'numpy')
+    target = np.ones(dim, dtype=np.float32) * 0.5
+
+    def fun(x):
+        x = (x - 0.5)* np.pi * 2
+        return 1 - np.sum(np.cos(10 * x) * np.exp(-x**2/2), axis=-1)
+
+    return fun, target
 
 objective_functions = {
     "rastrigin": get_rastrigin,
@@ -107,4 +166,10 @@ objective_functions = {
     "griewank": get_griewank,
     "rosenbrock": get_rosenbrock,
     "xin_she_yang": get_xin_she_yang,
+    "alpine01": alpine01,
+    "alpine02": alpine02,
+    "levy": levy,
+    "mishra": mishra,
+    "deflectedCorrugatedSpring": deflectedCorrugatedSpring,
+    "wavy": wavy,
 }
